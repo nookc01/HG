f98b38970ea5fb9d7bd29846c350e9937211fd48
nookc01 <chivisailam01@gmail.com>
2023-11-08 20:50:00 +0100

    first commit

---
+++ HAck game/HAck game/obj/iPhone/Release/Xamarin.iOS,Version=v1.0.AssemblyAttributes.fs
@@ -0,0 +1,3 @@
+namespace Microsoft.BuildSettings
+                [<System.Runtime.Versioning.TargetFrameworkAttribute("Xamarin.iOS,Version=v1.0", FrameworkDisplayName="Xamarin.iOS")>]
+                do ()
